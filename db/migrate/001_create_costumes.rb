f4b572c4e1056723eb381532b7195d9a1a6ba357
Rafael Lopez <lpz.raf@gmail.com>
2018-12-02 01:16:25 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,9 +13,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :url
+      t.string :image_url
       t.string :size
-      # t.timestamps
+      # t.timestamps null: false
     end
   end
 end
